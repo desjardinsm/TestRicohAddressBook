a5bff183684049c21a6c454b121ce9b542fbe55a
Matthew Desjardins <Desjardins.Matthew@gmail.com>
2024-02-06 13:18:13 -0500

    Test after_test

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,5 +1,8 @@
 Task test {
     $configuration = New-PesterConfiguration
     $configuration.Run.Throw = $true
+    $configuration.TestResult.Enabled = $true
+    $configuration.TestResult.OutputFormat = 'NUnit3'
+    $configuration.TestResult.OutputPath = 'results.nunit3.xml'
     Invoke-Pester -Configuration $configuration
 }
