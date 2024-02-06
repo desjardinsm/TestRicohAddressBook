c09ca7fcbf34b07ed6b2b43fd17f5c4d773eaf63
Matthew Desjardins <Desjardins.Matthew@gmail.com>
2024-02-06 14:04:00 -0500

    Clean up file output locations
    And remove debugging statements.

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,10 +1,14 @@
 Task test {
     $configuration = New-PesterConfiguration
 
+    $resultsDirectory = 'TestResults'
+
     $configuration.Run.Throw = $true
     $configuration.TestResult.Enabled = $true
+    $configuration.TestResult.OutputPath = Join-Path $resultsDirectory 'testResults.xml'
     $configuration.CodeCoverage.Enabled = $true
     $configuration.CodeCoverage.Path = './Module/'
+    $configuration.CodeCoverage.OutputPath = Join-Path $resultsDirectory 'coverage.xml'
 
     Invoke-Pester -Configuration $configuration
 }
