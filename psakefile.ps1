3b4033de9d283a60fe78567ef3db1e50bd78fe4c
Matthew Desjardins <Desjardins.Matthew@gmail.com>
2024-02-20 13:03:25 -0500

    Remove failing task

--- psakefile.ps1
+++ psakefile.ps1
@@ -27,10 +27,6 @@ Task Test {
     Invoke-Pester -Configuration $configuration
 }
 
-Task ThrowError {
-    throw 'Test failed task'
-}
-
 Task TagRelease {
     $module = Test-ModuleManifest -Path (Join-Path 'Module' 'RicohAddressBook.psd1')
     $tagName = "v$($module.Version)"
